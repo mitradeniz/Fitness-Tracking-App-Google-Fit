b87053e1a3b71b2693df545dcf1a78d617dc5452
mitradeniz <mitradenizkaratas@gmail.com>
2024-04-22 23:08:17 +0300

    3 chart data were pulled and "ChartSpecsActivity" was created for the chart details page

--- app/src/main/java/com/example/rwtcase/MainActivity.kt
+++ app/src/main/java/com/example/rwtcase/MainActivity.kt
@@ -6,16 +6,13 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.rwtcase.databinding.ActivityMainBinding
 import com.example.rwtcase.databinding.ActivityPermissionsBinding
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
@@ -28,7 +25,8 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.concurrent.TimeUnit
 
-internal lateinit var a: List<FitnessData>
+internal lateinit var a: MutableList<FitnessData>
+internal var listAll = mutableListOf<FitnessData>()
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -38,14 +36,6 @@ class MainActivity : AppCompatActivity() {
     private val REQUEST_CODE_ACCESS_FINE_LOCATION = 102
     private val REQUEST_CODE_BODY_SENSORS = 103
     private val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1111
-
-    private lateinit var barChart: BarChart
-    private lateinit var barData: BarData
-    private lateinit var barDataSet: BarDataSet
-    private lateinit var barEntryList: ArrayList<BarEntry>
-
-
-
 
     private val fitnessOptions = FitnessOptions.builder()
         .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
@@ -57,32 +47,6 @@ class MainActivity : AppCompatActivity() {
         bindingPermissionsBinding = ActivityPermissionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*
-        binding.bottomNav.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.page_1 -> {
-                    // Respond to navigation item 1 click
-//                    val intent1 = Intent(this@MainActivity, MainActivity::class.java)
-//                    startActivity(intent1)
-                    true
-                }
-                R.id.page_2 -> {
-                    // Respond to navigation item 2 click
-                    true
-                }
-                R.id.page_3 -> {
-                    // Respond to navigation item 2 click
-                    true
-                }
-                R.id.page_4 -> {
-                    // Respond to navigation item 2 click
-                    true
-                }
-                else -> false
-            }
-        }
-         */
-
         binding.mainActivityTo2Button.setOnClickListener {
             startActivity(Intent(this, MainActivity2::class.java))
         }
@@ -92,6 +56,11 @@ class MainActivity : AppCompatActivity() {
         val permissionRec = Manifest.permission.ACTIVITY_RECOGNITION
         val permissionBodySens = Manifest.permission.BODY_SENSORS
 
+
+/*        val gifImageView = findViewById<ImageView>(R.id.gifImageView)
+        gifImageView.setImageResource(R.drawable.loading_screen)
+
+        binding.gifImageView.setImageResource(R.drawable.loading_screen)*/
 
         if (ContextCompat.checkSelfPermission(this, permissionLoc) == PackageManager.PERMISSION_GRANTED &&  ContextCompat.checkSelfPermission(this, permissionRec) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, permissionBodySens) == PackageManager.PERMISSION_GRANTED) {
             // İzin verilmişse burada işlemleri gerçekleştirin
@@ -146,40 +115,37 @@ class MainActivity : AppCompatActivity() {
             .addOnSuccessListener { response ->
                 for (dataSet in response.buckets.flatMap { it.dataSets }) {
                     a = dumpDataSet(dataSet)
-
+                    listAll += a
                     for (i in a) {
-                        /*
-                        var count = 0
-                        if (i.type == "com.google.step_count.delta") {
-
-                            barEntryList.add(BarEntry(count.toFloat(), i.value.toFloat()))
-                            count += 1
-                        }
-
-                         */
                         Log.e("1", i.startTime.toString())
                         Log.e("2", i.endTime.toString())
                         Log.e("3", i.type.toString())
                         Log.e("4", i.value.toString())
-
                     }
-                    /*
-                    barDataSet = BarDataSet(barEntryList, "Adım:")
-                    barData = BarData(barDataSet)
-                    barChart = findViewById(R.id.mainActivityStepBarChart)
-                    barChart.data = barData
-                    barDataSet.color = Color.BLUE
-                    barDataSet.valueTextColor = Color.BLACK
-                    barDataSet.valueTextSize = 4f
-                    barChart.description.isEnabled = false
-
-                     */
-
                 }
             }
             .addOnFailureListener { e ->
                 Log.e("TAG", "There was an error reading data from Google Fit", e)
             }
+
+        // Uygulama yüklenmesi için beklenen süre
+        val splashTimeOut: Long = 15000 // 10 saniye
+
+        // Ana aktiviteye geçiş
+        val intent = Intent(this, MainActivity2::class.java)
+        // Splash ekranı tamamlanınca ana aktiviteye geç
+        Thread(Runnable {
+            kotlin.run {
+                try {
+                    Thread.sleep(splashTimeOut)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }).start()
 
     }
 
--- app/src/main/java/com/example/rwtcase/MainActivity2.kt
+++ app/src/main/java/com/example/rwtcase/MainActivity2.kt
@@ -1,15 +1,36 @@
 package com.example.rwtcase
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.rwtcase.databinding.ActivityMain2Binding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import java.time.LocalDateTime
 
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain2Binding
+
+    private lateinit var barChart: BarChart
+    private lateinit var barData: BarData
+    private lateinit var barDataSet: BarDataSet
+    private lateinit var barEntryListStep: ArrayList<BarEntry>
+    private lateinit var barEntryListCal: ArrayList<BarEntry>
+    private lateinit var barEntryListMin: ArrayList<BarEntry>
+
+
+    private lateinit var stepCountList: MutableList<Pair<Float, Float>>
+    private lateinit var calorieList: MutableList<Pair<Float, Float>>    // Gün bilgisi ve atılan adım tutulabilir
+    private lateinit var activeMinutesList: MutableList<Pair<Float, Float>>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
@@ -26,8 +47,8 @@ class MainActivity2 : AppCompatActivity() {
             when(item.itemId) {
                 R.id.page_1 -> {
                     // Respond to navigation item 1 click
-//                    val intent1 = Intent(this@MainActivity, MainActivity::class.java)
-//                    startActivity(intent1)
+                    // val intent1 = Intent(this@MainActivity, MainActivity::class.java)
+                    // startActivity(intent1)
                     true
                 }
                 R.id.page_2 -> {
@@ -45,5 +66,117 @@ class MainActivity2 : AppCompatActivity() {
                 else -> false
             }
         }
+
+        stepCountList = mutableListOf()
+        calorieList = mutableListOf()
+        activeMinutesList = mutableListOf()
+
+        for (i in listAll) {
+            when (i.type) {
+                "com.google.step_count.delta" -> {
+                    var x = i.startTime.dayOfMonth.toFloat()
+
+                    stepCountList.add(Pair(x.toFloat(), i.value.toFloat()))
+
+                }
+                "com.google.calories.expended" -> {
+                    var x = i.startTime.dayOfMonth.toFloat()
+
+                    calorieList.add(Pair(x.toFloat(), i.value.toFloat()))
+                }
+                "com.google.active_minutes" -> {
+                    var x = i.startTime.dayOfMonth.toFloat()
+
+                    activeMinutesList.add(Pair(x.toFloat(), i.value.toFloat()))
+                }
+            }
+        }
+
+        for ((index, pair) in stepCountList.withIndex()) {
+            Log.e("StepCountList", "Entry $index -> Start Time: ${(pair.first)}, Value: ${pair.second}")
+        }
+
+        for ((index, pair) in calorieList.withIndex()) {
+            Log.e("CalorieList", "Entry $index -> Start Time: ${(pair.first)}, Value: ${pair.second}")
+        }
+
+        for ((index, pair) in activeMinutesList.withIndex()) {
+            Log.e("ActiveMinutesList", "Entry $index -> Start Time: ${(pair.first)}, Value: ${pair.second}")
+        }
+
+        // Grafik 1 için barEntryList oluşturma
+        barEntryListStep = ArrayList()
+        for (pair in stepCountList) {
+            barEntryListStep.add(BarEntry(pair.first, pair.second))
+        }
+        barDataSet = BarDataSet(barEntryListStep, "Adım:")
+        barData = BarData(barDataSet)
+        barChart = findViewById(R.id.mainActivityStepBarChart)
+        barChart.data = barData
+        barDataSet.color = Color.YELLOW
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.valueTextSize = 0.25f
+        barChart.description.isEnabled = false
+
+// Grafik 2 için barEntryList oluşturma
+        barEntryListCal = ArrayList()
+        for (pair in calorieList) {
+            barEntryListCal.add(BarEntry(pair.first, pair.second))
+        }
+        barDataSet = BarDataSet(barEntryListCal, "Kalori:")
+        barData = BarData(barDataSet)
+        barChart = findViewById(R.id.mainActivityCalBarChart)
+        barChart.data = barData
+        barDataSet.color = Color.RED
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.valueTextSize = 0.25f
+        barChart.description.isEnabled = false
+
+
+// Grafik 3 için barEntryList oluşturma
+        barEntryListMin = ArrayList()
+        for (pair in activeMinutesList) {
+            barEntryListMin.add(BarEntry(pair.first, pair.second))
+        }
+        barDataSet = BarDataSet(barEntryListMin, "Hareket S.:")
+        barData = BarData(barDataSet)
+        barChart = findViewById(R.id.mainActivityMinChart)
+        barChart.data = barData
+        barDataSet.color = Color.BLUE
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.valueTextSize = 0.25f
+        barChart.description.isEnabled = false
+
+        binding.mainActivityStepBarChart.setOnClickListener {
+            // liste ile fonksiyonu çağır viewBinding ile sayfayı oluştur
+            // tek bir xml iskeleti yeterli olacak
+
+        }
+
+        binding.mainActivityCalBarChart.setOnClickListener {
+
+        }
+
+        binding.mainActivityMinChart.setOnClickListener {
+
+        }
+
+
+//            Log.e("1", i.startTime.toString())
+//            Log.e("2", i.endTime.toString())
+//            Log.e("3", i.type.toString())
+//            Log.e("4", i.value.toString())
+
     }
+
+    fun dateFormatter(date: LocalDateTime): String {
+        var resultTime = ""
+
+        resultTime += "${date.monthValue}-${date.dayOfMonth.toString()}-${date.year}"
+
+        return resultTime
+    }
+
 }
+
+
